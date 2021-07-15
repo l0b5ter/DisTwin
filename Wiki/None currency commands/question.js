8ff8a8fa26418fb17a10db1257b77464f4a1af0b
l0b5ter <44732631+l0b5ter@users.noreply.github.com>
2021-07-15 21:00:46 +0200

    Add files via upload

---
+++ Wiki/None currency commands/question.js
@@ -0,0 +1,51 @@
+const Discord = require("discord.js");
+const fetch = require('node-fetch');
+const fs = require("fs");
+const client = new Discord.Client();
+const { token, Prefix, QuestionInt, QuestionLink, ChannelQuestion, QuestionTitle } = require('../../config.json');
+
+module.exports.run = (client, message, args) => {
+	try {
+		let date = new Date();
+		client.setInterval(QuestionDay, QuestionInt);
+		message.channel.send("Bot is now hooked to the " + QuestionTitle + ". Every " + QuestionInt + "ms it will look for a new question!");
+		QuestionDay(message, client);
+	} catch(e) {
+		NoQuestion(message, client);
+	}
+};
+
+module.exports.help = {
+	name: "question",
+	desc: "Hook the question up with the api, so that it can grab a random question each time."
+};
+
+
+const QuestionDay = (message, client) => {
+	try {
+		fetch(QuestionLink).then(res => res.json())
+		.then(json => {	
+			let body = json.results;	  
+			body.forEach(function(item){
+				let embed = new Discord.RichEmbed()
+					.addField("Question of the day", item.question)
+					.setTimestamp()
+				client.channels.find(channel => channel.name === ChannelQuestion).send({embed: embed});
+				//console.log(item.question);
+			});			
+		});
+	} catch(e) {
+		
+		NoQuestion(message);
+	}
+};
+
+const NoQuestion = (message, client) => {
+		let date = new Date();
+		message.channel.send(CalendarLink+ '/' +date.getDate()+ '.json');
+		let embed = new Discord.RichEmbed()
+			.setColor(0xfef08d)
+			.setTitle("Couldnt grab question!")
+			.addField("+--------------------------------------------------+", "Make sure the link is set right.")
+		client.channels.find(channel => channel.name === ChannelQuestion).send({embed: embed});
+};
