40ecc0e98c84c53e93b89b49250ca1ae5c7f74e1
l0b5ter <44732631+l0b5ter@users.noreply.github.com>
2021-09-11 16:07:20 +0200

    Add files via upload

---
+++ Wiki/Twitch/ReedemAble/update.js
@@ -0,0 +1,44 @@
+const tmi = require("tmi.js");
+const robot = require('robotjs');
+const fetch = require('node-fetch');
+const path = require("path");
+var request = require('request');
+
+const CustomeRewardID = '';
+const BroadCasterID = '';
+
+const headers = {
+	'client-id': '',
+	'Authorization': 'Bearer ',
+	'Content-Type': 'application/json' 
+};
+
+module.exports.run = (client, channel, user, message, self) => {
+	var bodyS = {
+		"cost": 60,
+		"prompt": "Owned by: "+ user['display-name']
+	};
+	var options = {
+		url: 'https://api.twitch.tv/helix/channel_points/custom_rewards?broadcaster_id=' + BroadCasterID + '&id=' + CustomeRewardID,
+		method: 'PATCH',
+		headers: headers,
+		json: true,
+		body: bodyS
+	};
+	try {
+		request(options, callback);
+	} catch(e) {
+		console.log(e);
+	}
+};
+
+module.exports.help = {
+	name: "update",										//Custome-reward-id/msg-id for the right reedemable
+	desc: "Update a CustomReward with its custom-reward-id"					//Description
+};
+
+const callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+        console.log("custom-reward-id UPDATED");
+    }
+}
